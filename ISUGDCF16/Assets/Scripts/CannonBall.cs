5d4cc2986860dc827b86ef0610a4c3c961a1f1f8
Austin Kerr <akerr@iastate.edu>
2016-10-22 00:34:39 -0500

    Hello

---
+++ ISUGDCF16/Assets/Scripts/CannonBall.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonBall : MonoBehaviour {
+	public GameObject prefab;
+    public Transform target;
+    // Update is called once per frame
+    IEnumerator Start()
+    {
+
+        yield return new WaitForSeconds(Random.Range(0f,2f));
+        
+        while (true)
+        {
+            if (target)
+                transform.LookAt(target);
+            float thrust = 1500f;
+            GameObject projectile = (GameObject)Instantiate(prefab, transform.position, transform.rotation);
+            projectile.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * thrust);
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            target = other.gameObject.transform;
+        }
+    }
+}
