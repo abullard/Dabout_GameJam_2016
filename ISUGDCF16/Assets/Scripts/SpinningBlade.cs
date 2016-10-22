a0122fbe67a2a7c3682c3ef8434c8baf6fa6272b
joemussa <joemussa@gmail.com>
2016-10-22 12:13:22 -0500

    Fixed merge conflicts

---
+++ ISUGDCF16/Assets/Scripts/SpinningBlade.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinningBlade : MonoBehaviour {
+	
+	public Rigidbody rb;
+	public float torque;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent <Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate(){
+		rb.AddRelativeTorque (Vector3.up * torque);
+		rb.AddForce (Vector3.forward * 1.0f, ForceMode.Acceleration);
+	}
+
+	void Update () {
+		
+	}
+}
