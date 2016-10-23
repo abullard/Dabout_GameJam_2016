e8d0c49af47031e9039af2ef97beb8cd69e7d851
Austin Kerr <akerr@iastate.edu>
2016-10-23 01:00:55 -0500

    Pushing to add

--- ISUGDCF16/Assets/Scripts/SettingSun.cs
+++ ISUGDCF16/Assets/Scripts/SettingSun.cs
@@ -2,14 +2,46 @@
 using System.Collections;
 
 public class SettingSun : MonoBehaviour {
+    public float startX = 0f;
+    public float startY = 1f;
+    public float startZ = -15f;
+    public float endX = 0f;
+    public float endY = 1f;
+    public float endZ = 15f;
+    public float trajectoryHeight = 10f;
+    Vector3 startPos;
+    Vector3 endPos;
+    Vector3 curPos;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        startPos = new Vector3(startX, startY, startZ);
+        endPos = new Vector3(endX, endY, endZ);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if(curPos.z == startZ)
+        {
+            startPos = new Vector3(startX, startY, startZ);
+            endPos = new Vector3(endX, endY, endZ);
+        }
+        if (curPos.z == endZ)
+        {
+            startPos = new Vector3(endX, endY, endZ);
+            endPos = new Vector3(startX, startY, startZ);
+        }
+
+        //get the current time within the lerping time range
+        float cTime = Time.time * .2f;
+
+        //calculate the straight line lerp position
+        curPos = Vector3.Lerp(startPos, endPos, cTime);
+
+        //add a value to Y, using Sine to give a curved trajectory in the Y direction
+        curPos.y += trajectoryHeight * Mathf.Sin(Mathf.Clamp01(cTime) * Mathf.PI);
+
+        //assign new position to the game object
+        transform.position = curPos;
+    }
 }
