b2672fc911b9f46b10015527c7af9b40e72b5b77
Austin Bullard <austinbullard@protonmail.ch>
2016-10-23 15:20:29 -0500

    main scene done

---
+++ ISUGDCF16/Assets/Scripts/WaterRespawn.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterRespawn : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Player.player.respawn();
+        }
+    }
+}
