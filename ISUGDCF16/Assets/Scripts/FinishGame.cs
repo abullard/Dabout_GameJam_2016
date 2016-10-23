e9c191576759490d9b27925fd0bd3f92ac8af7bd
Austin Bullard <austinbullard@protonmail.ch>
2016-10-23 16:16:38 -0500

    Finished main scene, working on main menu

--- ISUGDCF16/Assets/Scripts/FinishGame.cs
+++ ISUGDCF16/Assets/Scripts/FinishGame.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class FinishGame : MonoBehaviour {
 
     public GameObject timerText;
+    public Text winText;
 
     //if the player enters the collider, trigger the finish game event
     void OnTriggerEnter(Collider other) {
         if(other.gameObject.CompareTag("Player")) {
            timerText.SendMessage("finished");
+            winText.color = Color.green;
+            winText.text = "GOAL!";
         }
     }
 }
